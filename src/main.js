727cb6e024953773ed2f10b794d7f718aba2a927
Wallace Barros da Cruz <104733992+Wallacebarros@users.noreply.github.com>
2024-02-28 22:18:38 +0000

    adicionando uma logica que simula  a autenticação de login do ususario

---
+++ src/main.js
@@ -0,0 +1,43 @@
+const userName = document.querySelector('#userName')
+const password = document.querySelector('#password')
+const span = document.querySelectorAll('.login__span')
+const btn = document.querySelector('#btn')
+
+const database = {
+    userName : 'Stive',
+    password : '12345'
+}
+
+function userNameAutentication() {
+    if (userName.value == database.userName) {
+        // autenticado
+        span[0].style = 'display: none'
+        return true
+    }
+    else {
+        span[0].style = 'display: block'
+        return false
+    }
+}
+
+function passwordAutentication() {
+    if (password.value == database.password) {
+        // autenticado
+        span[1].style = 'display: none'
+        return true
+    }
+    else {
+        span[1].style = 'display: block'
+    }
+}
+
+// console.log(btn.firstChild);
+
+function clicar () {
+    // let teste = userNameAutentication()
+    if (! userNameAutentication()) return 
+
+    if (! passwordAutentication()) return
+
+    btn.firstChild.href = './bemvindo.html'
+}
